76f40c1e8e9487b548a264642d4102304b7c1e67
cecilioAlvarez <cmac@arquitecturajava.com>
2023-10-11 12:11:22 +0200

    nuevos

---
+++ src/main/java/com/mongodb/demo/models/Cliente.java
@@ -0,0 +1,38 @@
+package com.mongodb.demo.models;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document("clientes")
+public class Cliente {
+
+    private int id;
+    private String nombre;
+    private String apellidos;
+    private int edad;
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getApellidos() {
+        return apellidos;
+    }
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+    public int getEdad() {
+        return edad;
+    }
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    
+    
+}
